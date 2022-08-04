f67a97de46afdd67800075d48e65c71e08e3fb34
pwf.com <109667170+pwfcom@users.noreply.github.com>
2022-08-04 21:27:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-echo "======== docker containers logs file size ========"  
-
-logs=$(find /var/lib/docker/containers/ -name *-json.log)  
-
-for log in $logs  
-        do  
-             ls -lh $log   
+echo "======== docker containers logs file size ========"
+logs=$(find /var/lib/docker/containers/ -name *-json.log)
+for log in $logs
+        do
+             ls -lh $log
         done
 #####
-echo "======== start clean docker containers logs ========"  
-
-logs=$(find /var/lib/docker/containers/ -name *-json.log)  
-
-for log in $logs  
-        do  
-                echo "clean logs : $log"  
-                cat /dev/null > $log  
-        done  
-
-echo "======== end clean docker containers logs ========"  
+echo "======== start clean docker containers logs ========"
+logs=$(find /var/lib/docker/containers/ -name *-json.log)
+for log in $logs
+        do
+                echo "clean logs : $log"
+                cat /dev/null > $log
+        done
+echo "======== end clean docker containers logs ========"
