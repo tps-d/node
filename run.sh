6c52ca5472fc409c853c30cdc552609e30ed80eb
pwf.com <109667170+pwfcom@users.noreply.github.com>
2022-08-04 21:28:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,3 @@ for log in $logs
         do
              ls -lh $log
         done
-#####
-echo "======== start clean docker containers logs ========"
-logs=$(find /var/lib/docker/containers/ -name *-json.log)
-for log in $logs
-        do
-                echo "clean logs : $log"
-                cat /dev/null > $log
-        done
-echo "======== end clean docker containers logs ========"
