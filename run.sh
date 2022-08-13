72aa2564163df33c9223528b330580fec3c146ca
pwf.com <109667170+pwfcom@users.noreply.github.com>
2022-08-13 23:58:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
+echo -n "enter mode:"
+read mode
 
+if ["$mode" -eq "clear"]; then
 echo "======== docker containers logs file size ========" >>/root/run.log
-
 logs=$(find /var/lib/docker/containers/ -name *-json.log)  
-
 for log in $logs  
-        do  
+        do 
              du -b $log
         done
 echo "======== start clean docker containers logs ========"  >>/root/run.log
-
 logs=$(find /var/lib/docker/containers/ -name *-json.log)  
-
 for log in $logs  
         do  
                 echo "clean logs : $log"  
                 cat /dev/null > $log  
-        done  
-
+        done
 echo "======== end clean docker containers logs ========" >>/root/run.log
 cat /root/run.log
+if
